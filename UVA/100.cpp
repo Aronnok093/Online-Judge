956ebb2d7ffef3da38b6185e6cbe812f751a156a
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2020-11-23 02:14:42 +0600

    Create 100.cpp

---
+++ UVA/100.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+    *
+    * Prosen Ghosh
+    * American International University - Bangladesh (AIUB)
+    *
+*/
+int main(){
+
+    int a,b;
+    while((scanf("%d %d",&a,&b)) == 2){
+        int mx = 0;
+        int x = (a>b)?a:b;
+        int y = (a<b)?a:b;
+        for(int i = y; i <= x; i++){
+        int n = i,cnt = 1;
+            while(n > 1){
+                if(n%2)n = 3*n+1;
+                else n/=2;
+                cnt++;
+            }
+            if(cnt >= mx)mx = cnt;
+        }
+        cout << a << " " << b << " " << mx << endl;
+    }
+    return 0;
+}
