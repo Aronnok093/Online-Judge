c18807bd0a8347c0efdcffe917592fff5d5f684a
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2020-11-24 20:37:16 +0600

    Create 4A-Watermelon.c

---
+++ Code Force/4A-Watermelon.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int weight;
+    scanf("%d",&weight);
+    if(weight>2&&weight%2==0){
+        printf("YES\n");
+    }
+    else{
+        printf("NO\n");
+    }
+    return 0;
+
+
+}
