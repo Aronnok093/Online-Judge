b56ef8abaff3ad83b4c5d2c8477018e6f3d9bdb9
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2020-11-26 20:34:38 +0600

    Create 1182.c

---
+++ Light oj/1182.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int t;
+
+	scanf("%d", &t);
+
+	int n;
+	int count;
+
+	for (int cs = 1; cs <= t; cs++) {
+		scanf("%d", &n);
+		count = 0;
+		while(n) {
+			count += n % 2;
+			n = n / 2;
+		}
+
+		if(count % 2) {
+			printf("Case %d: odd\n", cs);
+		}
+
+		else {
+			printf("Case %d: even\n", cs);
+
+		}
+
+	}
+
+
+}
