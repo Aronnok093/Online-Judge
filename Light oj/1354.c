0d9a5cc0520b74c362051cabe369d845cfd6d5ae
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2020-11-30 18:13:14 +0600

    Update 1354.c

--- Light oj/1354.c
+++ Light oj/1354.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
-#include <math.h>
-bool check(int x,int y)
-{  int sum=0,i=0;
-   while(y>0)
-{  int mod=y%10;
-   sum+=mod*pow(2,i++);
-   y/=10;
-}
-   if(x!=sum) return false;
-   return true;
+int DecBin(int n)
+{
+        int multiplier = 1,result = 0;
+        while(n>0)
+        {
+
+        result+=n%2*multiplier;
+        multiplier*=10;
+        n/=2;
+
+        }
+        return result;
+
+
 }
 int main()
 {
-   int n,kase=1;
-   scanf("%d",&n);
-   while(n--)
-   { int a,b,c,d,a1,b1,c1,d1;
-     scanf("%d.%d.%d.%d",&a,&b,&c,&d);
-     scanf("%d.%d.%d.%d",&a1,&b1,&c1,&d1);
-     if( check(a,a1) && check(b,b1) && check(c,c1) && check(d,d1)) printf("Case %d: Yes\n",kase++);
-     else printf("Case %d: No\n",kase++);
-     }
-     return 0;
-    }
+        int T,da,db,dc,dd,ba,bb,bc,bd;
+        scanf("%d",&T);
+        int i;
+        for(i =0; i<T;i++)
+        {
+                scanf("%d.%d.%d.%d",&da,&db,&dc,&dd);
+                scanf("%d.%d.%d.%d",&ba,&bb,&bc,&bd);
+
+                if(DecBin(da)==ba&&DecBin(db)==bb&&DecBin(dc)==bc&&DecBin(dd)==bd)
+                printf("Case %d: Yes\n",i+1);
+                else
+                printf("Case %d: No\n",i+1);
+
+        }
+        return 0;
+
+
+}
