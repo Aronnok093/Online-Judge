3287f976d2683de879a339d484ddf71fb82c538f
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2020-12-04 19:24:56 +0600

    Update 11777 - Automate the Grades.c

--- UVA/11777 - Automate the Grades.c
+++ UVA/11777 - Automate the Grades.c
@@ -1,32 +1,30 @@
 #include<stdio.h>
-int main()
-{
-    int t1,t2,f,at,ct1,ct2,ct3,c,d,n,i;
-    while(scanf("%d",&n)==1)
-    {
-        for(i=1; i<=n; i++)
-        {
-           scanf("%d%d%d%d%d%d%d",&t1,&t2,&f,&at,&ct1,&ct2,&ct3);
-        if(ct1<=ct2 && ct1<=ct3)
-           c=(ct2+ct3)/2;
-        else if(ct2<=ct1 && ct2<=ct3)
-           c=(ct1+ct3)/2;
-        else if(ct3<=ct1 && ct3<=ct2)
-             c=(ct1+ct2)/2;
-        d=t1+t2+f+at+c;
-        if(d>=90)
-        printf("Case %d: A\n",i);
-        else if(d>=80)
-        printf("Case %d: B\n",i);
-        else if(d>=70)
-        printf("Case %d: C\n",i);
-        else if(d>=60)
-        printf("Case %d: D\n",i);
-        else if(d<60)
-        printf("Case %d: F\n",i);
+int main(){
+    int tr1,tr2,at,fe,ct1,ct2,ct3,i,term,c,sum;
+    while(scanf("%d",&term)==1){
 
-        }
-    }
+       for(i=1;i<=term;i++){
 
-    return 0;
+        scanf("%d%d%d%d%d%d%d",&tr1,&tr2,&fe,&at,&ct1,&ct2,&ct3);
+        if(ct1<=ct2&&ct1<=ct3)
+            c=(ct2+ct3)/2;
+        else if(ct2<=ct1&&ct2<=ct3)
+            c=(ct1+ct3)/2;
+        else if(ct3<=ct1&&ct3<=ct2)
+            c=(ct1+ct2)/2;
+        sum=tr1+tr2+at+fe+c;
+        if(sum>=90)
+            printf("Case %d: A\n",i);
+        else if(sum>=80)
+            printf("Case %d: B\n",i);
+        else if(sum>=70)
+            printf("Case %d: C\n",i);
+        else if(sum>=60)
+            printf("Case %d: D\n",i);
+        else if(sum<60)
+            printf("Case %d: F\n",i);
+
+                            }
+                    }
+                return 0;
 }
