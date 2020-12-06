8ed31e087cc998c7102fb50ffe8baf5a09d7c98f
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2020-12-07 01:12:53 +0600

    Add files via upload

---
+++ Code Force/A. Avoid Trygub.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int term;
+    cin>>term;
+    while(term--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        sort(s.begin(),s.end());
+        cout<<s<<endl;
+
+    }
+    return 0;
+
+}
