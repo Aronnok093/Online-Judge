2e105d58844d347d62c90f0e9aa50814b0cdc4f9
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2020-12-10 22:34:41 +0600

    Add files via upload

---
+++ Toph/Replay of Virtual Techfest 2020 Regional Programming Contest/G.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#define pi 2*acos(0.0)
+using namespace std;
+int main()
+{
+    cout<<fixed;
+    cout<<setprecision(10);
+    int t;
+    cin>>t;
+    while(t--){
+        double area,r,x,ang,tri;
+        cin>>r>>x;
+        ang=(pi*x)/360;
+        tri=r*r*tan(ang);
+        area=(pi*r*r*x)/360;
+        cout<<(tri-area)<<endl;
+    }
+    return 0;
+}
