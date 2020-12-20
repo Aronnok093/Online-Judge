6a4172a86aafb4c683543e9b014bde3da333c422
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2020-12-20 23:11:48 +0600

    Update B. Fair Numbers.cpp

--- Code Force/B. Fair Numbers.cpp
+++ Code Force/B. Fair Numbers.cpp
@@ -1,1 +1,42 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    unsigned long long int n,m,temp;
+    long long int t;
+    bool ans;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        m=n;
+        for(long long int i=n; ;i++){
+                ans=true;
+                temp=i;
+            while(temp!=0){
+                if(temp%10==0)
+                    temp=temp/10;
+                else if(temp%10==0){
+                    ans=false;
+                    break;
+
+                }
+                else if(i%(temp%10)!=0){
+                    ans=false;
+                    break;
+                }
+                temp=temp/10;
+
+            }
+            if(ans){
+                cout<<i<<endl;
+                break;
+            }
+        }
+
+
+    }
+    return 0;
+
+}
