295db5e260975e587686a3ce8eee1ce3bbd3c43e
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2021-01-23 04:17:12 +0600

    Add files via upload

---
+++ CSES/Weird Algorithm.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define fs() ios::sync_with_stdio(0); cin.tie(0);
+using namespace std;
+int main(){
+    fs()
+    long long  n;
+    cin>>n;
+    cout<<n<<" "<<endl;
+    while(n!=1){
+        if(n%2==0){
+            n/=2;
+            cout<<n<<" ";
+        }
+        else
+        {
+            n=(n*3)+1;
+            cout<<n<<" ";
+        }
+    }
+    cout<<endl;
+    return 0;
+}
