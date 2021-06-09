c2b71d81916457a482079df3a78ddecb17e0b41a
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2021-06-10 04:19:50 +0600

    add

---
+++ Toph/Training Contest/D. Guess the Number.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t=1,n,ans,x;
+    //while(t--)
+    //{
+       cin>>n;
+
+       cout<<"? "<<1<<endl;
+       cin>>x;
+
+       if(x==0) ans=1;
+       else ans=1+x;
+
+
+       cout<<"! "<<ans<<endl;
+
+       return 0;
+
+    //}
+}
