0269c982f245d1f4c486bf2715ee5f1b38ce0be4
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2021-07-04 21:15:18 +0600

    Add files via upload

---
+++ At Coder/ABC 208/A - Rolling Dice .cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+                    //A*6>=b && b>=a
+
+    if(a*6>=b&&b>=a)
+        cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
+
+
+    return 0;
+
+
+}
